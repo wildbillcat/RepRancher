9cd1805bb99846277fbf1ccf8ea631f1d2791ef9
Patrick McMorran <patrick.mcmorran@yale.edu>
2013-12-12 17:04:02 -0500

    fiddling still trying to get conveyor to say world...

--- RepRancher/Program.cs
+++ RepRancher/Program.cs
@@ -22,10 +22,11 @@ namespace RepRancher
             rpcCall.Add(new JProperty("jsonrpc", "2.0"));
             rpcCall.Add(new JProperty("id", "1"));
             rpcCall.Add(new JProperty("method", "hello"));
+            //rpcCall.Add(new JProperty("method", "add"));
 
-            Dictionary<int, string> Params = new Dictionary<int, string>();
+            List<object> Params = new List<object>();
             // params is a collection values which the method requires..
-            if (Params.Keys.Count == 0)
+            if (Params.Count == 0)
             {
                 rpcCall.Add(new JProperty("params", new JArray()));
             }
@@ -33,22 +34,34 @@ namespace RepRancher
             {
                 JArray props = new JArray();
                 // add the props in the reverse order!
-                for (int i = Params.Keys.Count - 1; i >= 0; i--)
+                foreach(string param in Params)
                 {
                     // add the params
-                    props.Add(Params[i]);
+                    props.Add(param);
                 }
                 rpcCall.Add(new JProperty("params", props));
             }
 
             // serialize json for the request
             string s = JsonConvert.SerializeObject(rpcCall);
+            byte[] byteArray = Encoding.UTF8.GetBytes(s);
+            
 
             CookieContainer CookieContainer = new CookieContainer();
             HttpWebRequest HttpRequest = WebRequest.CreateHttp("http://localhost:9999");
-            HttpRequest.ContentType = "text/json";
+            //HttpWebRequest HttpRequest = WebRequest.CreateHttp("http://validate.jsontest.com/");
+            //HttpWebRequest HttpRequest = WebRequest.CreateHttp("http://www.raboof.com/projects/jayrock/demo.ashx");
+            HttpRequest.ContentType = "application/json";
             HttpRequest.Method = "POST";
             HttpRequest.CookieContainer = CookieContainer;
+
+            HttpRequest.ContentLength = byteArray.Length;
+            using (Stream dataStream = HttpRequest.GetRequestStream())
+            {
+                dataStream.Write(byteArray, 0, byteArray.Length);
+                dataStream.Close();
+            }
+
             HttpWebResponse HttpResponse;
             try
             {
