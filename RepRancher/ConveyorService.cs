7e2d00d3d9fb572a7967547565a3cf42b946d8d6
Patrick McMorran <patrick.mcmorran@yale.edu>
2013-12-13 17:03:35 -0500

    Working on abstracting the interface to Conveyor!

---
+++ RepRancher/ConveyorService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace RepRancher
+{
+    class ConveyorService
+    {
+        IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9999);
+        TcpClient tcpClient;
+        Stream dataStream;
+
+        ConveyorService(string IPaddress, int PortNumber)
+        {
+            ipEndPoint = new IPEndPoint(IPAddress.Parse(IPaddress), PortNumber);
+            tcpClient = new TcpClient();
+            tcpClient.Connect(ipEndPoint);
+            dataStream = tcpClient.GetStream();
+
+        }
+    }
+}
---
+++ RepRancher/JsonRpcResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepRancher
+{
+    class JsonRpcResult
+    {
+        //Example Reply	"{\"jsonrpc\": \"2.0\", \"result\": \"world\", \"id\": \"2\"}"	string
+        public double jsonrpc { get; set; }
+        public string result { get; set; }
+        public int id { get; set; }
+    }
+}
