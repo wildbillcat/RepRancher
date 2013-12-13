cd1404b83c76165c70e29f91bcb47e8c26d3a115
Patrick McMorran <patrick.mcmorran@yale.edu>
2013-12-13 15:56:48 -0500

    Finally got it to talk to Conveyor. Failed to realize it's just a TCP connection.

--- RepRancher/Program.cs
+++ RepRancher/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
+using System.Net.Sockets;
 using System.IO;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
@@ -20,7 +21,7 @@ namespace RepRancher
 
             JObject rpcCall = new JObject();
             rpcCall.Add(new JProperty("jsonrpc", "2.0"));
-            rpcCall.Add(new JProperty("id", "1"));
+            rpcCall.Add(new JProperty("id", "2"));
             rpcCall.Add(new JProperty("method", "hello"));
             //rpcCall.Add(new JProperty("method", "add"));
 
@@ -44,39 +45,24 @@ namespace RepRancher
 
             // serialize json for the request
             string s = JsonConvert.SerializeObject(rpcCall);
-            byte[] byteArray = Encoding.UTF8.GetBytes(s);
-            
+            byte[] byteArray = Encoding.ASCII.GetBytes(s);
 
-            CookieContainer CookieContainer = new CookieContainer();
-            HttpWebRequest HttpRequest = WebRequest.CreateHttp("http://localhost:9999");
-            //HttpWebRequest HttpRequest = WebRequest.CreateHttp("http://validate.jsontest.com/");
-            //HttpWebRequest HttpRequest = WebRequest.CreateHttp("http://www.raboof.com/projects/jayrock/demo.ashx");
-            HttpRequest.ContentType = "application/json";
-            HttpRequest.Method = "POST";
-            HttpRequest.CookieContainer = CookieContainer;
-
-            HttpRequest.ContentLength = byteArray.Length;
-            using (Stream dataStream = HttpRequest.GetRequestStream())
+            using (TcpClient tcpClient = new TcpClient())
             {
-                dataStream.Write(byteArray, 0, byteArray.Length);
-                dataStream.Close();
+                IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+                IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 9999);
+                tcpClient.Connect(ipEndPoint);
+                string Reply;
+                using (Stream dataStream = tcpClient.GetStream())
+                {
+                    dataStream.Write(byteArray, 0, byteArray.Length);
+                    byte[] bytesToRead = new byte[tcpClient.ReceiveBufferSize];
+                    int bytesRead = dataStream.Read(bytesToRead, 0, tcpClient.ReceiveBufferSize);
+                    Reply = Encoding.ASCII.GetString(bytesToRead, 0, bytesRead);
+                    Console.WriteLine(Reply);
+                }
             }
 
-            HttpWebResponse HttpResponse;
-            try
-            {
-                HttpResponse = (HttpWebResponse)HttpRequest.GetResponse();
-            }
-            catch (WebException error)
-            {
-                HttpResponse = (HttpWebResponse)error.Response;
-            }
-            string Reply;
-            using (var streamReader = new StreamReader(HttpResponse.GetResponseStream()))
-            {
-               Reply = streamReader.ReadToEnd();
-            }
-            Console.WriteLine(Reply);
         }
     }
 }
