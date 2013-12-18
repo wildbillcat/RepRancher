180d3286869f5b7edd222515b5efdd1040f9a53b
Patrick McMorran <patrick.mcmorran@yale.edu>
2013-12-18 16:55:49 -0500

    Cleaning up the Conveyor Service, working on creating two threaded program, on to read/respond to conveyor and another to monitor makerfarm/Issue commands to conveyor.

--- RepRancher/ConveyorService.cs
+++ RepRancher/ConveyorService.cs
@@ -6,22 +6,49 @@ using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
 using System.IO;
+using System.Threading;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace RepRancher
 {
     class ConveyorService
     {
-        IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9999);
+        IPEndPoint ipEndPoint;
         TcpClient tcpClient;
         Stream dataStream;
 
-        ConveyorService(string IPaddress, int PortNumber)
+        public ConveyorService(string IPaddress, int PortNumber)
         {
             ipEndPoint = new IPEndPoint(IPAddress.Parse(IPaddress), PortNumber);
             tcpClient = new TcpClient();
             tcpClient.Connect(ipEndPoint);
             dataStream = tcpClient.GetStream();
+        }
+    }
 
+    class ConveyorListenerService
+    {
+        TcpClient tcpClient;
+        Stream dataStream;
+
+        public ConveyorListenerService(TcpClient TcpClient, Stream DataStream)
+        {
+            tcpClient = TcpClient;
+            dataStream = DataStream;
+        }
+
+        public void ThreadRun()
+        {
+            while (true)
+            {
+                byte[] bytesToRead = new byte[tcpClient.ReceiveBufferSize];
+                int bytesRead = dataStream.Read(bytesToRead, 0, tcpClient.ReceiveBufferSize);
+                string Reply = Encoding.ASCII.GetString(bytesToRead, 0, bytesRead);
+                JsonRpcResult Result = JsonConvert.DeserializeObject<JsonRpcResult>(Reply);
+                Console.WriteLine(Result.result);
+                
+            }
         }
     }
 }
