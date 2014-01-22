a0afccf471b6bc3ee04e516875e075714d2ac107
Patrick McMorran <patrick.mcmorran@yale.edu>
2014-01-22 16:40:53 -0500

    Started working on Server Write API Tests

---
+++ Conveyor_JSONRPC_API_Tests/ServerAPIWriteTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using Conveyor_JSONRPC_API;
+using Conveyor_JSONRPC_API.Types;
+
+namespace Conveyor_JSONRPC_API_Tests
+{
+    [TestClass]
+    public class ServerAPIWriteTests
+    {
+
+        IPEndPoint ipEndPoint;
+        TcpClient tcpClient;
+        Stream dataStream;
+        int rpcid;
+
+        public ServerAPIWriteTests()
+        {
+            ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9999);
+            tcpClient = new TcpClient();
+            tcpClient.Connect(ipEndPoint);
+            dataStream = tcpClient.GetStream();
+            rpcid = 0;
+        }
+
+        [TestMethod]
+        public void Hello()
+        {
+            ServerAPI.Hello(rpcid++);
+        }
+
+
+    }
+}
