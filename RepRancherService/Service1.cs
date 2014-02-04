bdabc3775fbf1aeeede6e105ad23b96c76e48e09
Patrick McMorran <patrick.mcmorran@yale.edu>
2014-02-04 17:04:36 -0500

    updated RepRancher installer, added autorestart when conveyor failure detected

--- RepRancherService/Service1.cs
+++ RepRancherService/Service1.cs
@@ -23,11 +23,8 @@ namespace RepRancherService
 
         protected override void OnStart(string[] args)
         {
-            Conveyor = new ConveyorService("127.0.0.1", 9999);
-            GC.KeepAlive(Conveyor);
             t = new System.Timers.Timer(5000);
-            t.Elapsed += new System.Timers.ElapsedEventHandler(HealthCheck);
-            Health = new System.Timers.Timer(10000);
+            t.Elapsed += new System.Timers.ElapsedEventHandler(startup);
             t.Start();
         }
 
@@ -42,19 +39,24 @@ namespace RepRancherService
                 Conveyor = new ConveyorService("127.0.0.1", 9999);
                 Conveyor.Startup();
             }
-            
+            Health.Enabled = true;
+            Health.Start();
         }
 
         public void startup(object source, System.Timers.ElapsedEventArgs e)
         {
             t.Stop();
             t.Enabled = false;
+            Conveyor = new ConveyorService("127.0.0.1", 9999); 
+            GC.KeepAlive(Conveyor);
             Conveyor.Startup();
+            Health = new System.Timers.Timer(10000);
+            Health.Elapsed += new System.Timers.ElapsedEventHandler(HealthCheck);
         }
 
         protected override void OnStop()
         {
-            Conveyor.Dispose();
+            //Conveyor.Dispose();
         }
     }
 }
