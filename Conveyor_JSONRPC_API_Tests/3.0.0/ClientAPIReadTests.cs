44ee0f97370219e6a2821860ea80412d424d52f6
Patrick McMorran <patrick.mcmorran@yale.edu>
2014-03-17 18:06:58 -0400

    Corrected a typo.

--- Conveyor_JSONRPC_API_Tests/3.0.0/ClientAPIReadTests.cs
+++ Conveyor_JSONRPC_API_Tests/3.0.0/ClientAPIReadTests.cs
@@ -41,7 +41,7 @@ namespace Conveyor_JSONRPC_API_Tests._3._0._0
         }
 
         [TestMethod]
-        public void ReachMachineStateChanged()
+        public void ReadMachineStateChanged()
         {
             string JSON = "{\"params\": {\"can_print\": true, \"number_of_toolheads\": 2, \"has_heated_platform\": true, \"display_name\": \"Rep2X\", \"name\": {\"iserial\": \"7523733353635171A221\", \"pid\": 45077, \"port_type\": \"UsbPort\", \"vid\": 9153}, \"printer_type\": \"The Replicator 2X\", \"profile_name\": \"Replicator2X\", \"toolhead_target_temperature\": null, \"build_volume\": [246, 152, 155], \"state\": \"PAUSED\", \"driver_name\": \"s3g\", \"machine_names\": [\"TheReplicator2X\"], \"firmware_version\": [7, 5], \"temperature\": null}, \"jsonrpc\": \"2.0\", \"method\": \"machine_state_changed\"}";
             string methodname = ConveyorJsonReplyParser.GetMethodName(JSON);
@@ -57,7 +57,7 @@ namespace Conveyor_JSONRPC_API_Tests._3._0._0
         }
 
         [TestMethod]
-        public void ReachMachineTemperatureChanged()
+        public void ReadMachineTemperatureChanged()
         {
             string JSON = "{\"params\": {\"can_print\": true, \"number_of_toolheads\": 2, \"has_heated_platform\": true, \"display_name\": \"Rep 2X\", \"name\": {\"iserial\": \"7523733353635171A221\", \"pid\": 45077, \"port_type\": \"UsbPort\", \"vid\": 9153}, \"printer_type\": \"The Replicator 2X\", \"profile_name\": \"Replicator2X\", \"toolhead_target_temperature\": null, \"build_volume\": [246, 152, 155], \"state\": \"IDLE\", \"driver_name\": \"s3g\", \"machine_names\": [\"TheReplicator2X\"], \"firmware_version\": [7, 5], \"temperature\": null}, \"jsonrpc\": \"2.0\", \"method\": \"machine_temperature_changed\"}";
             string methodname = ConveyorJsonReplyParser.GetMethodName(JSON);
