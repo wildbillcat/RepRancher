0c2dfd5f226ef1ac857c1a61b721543b11a18d3c
Patrick McMorran <patrick.mcmorran@yale.edu>
2014-03-18 18:18:29 -0400

    Updated Interface with parameter

--- RepRancher/Rancher.cs
+++ RepRancher/Rancher.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RepRancher.MakerFarmService;
 
 namespace RepRancher
 {
@@ -13,6 +14,6 @@ namespace RepRancher
         RancherCommandGlossary GetRancherCommandGlossary();
 
         //This returns an array of Makerfarm Updates that can be sent to MakerFarm.
-        MakerFarmUpdate[] GetMakerFarmUpdates();
+        MakerFarmUpdate[] GetMakerFarmUpdates(MachineInterest[] ReportOn);
     }
 }
