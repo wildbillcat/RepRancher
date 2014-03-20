f64c90a2d1ebf9a360e874fadcb4244106b79168
Patrick McMorran <patrick.mcmorran@yale.edu>
2014-03-20 12:39:21 -0400

    Missed new classes!

--- RepRancher/RanchForeman.cs
+++ RepRancher/RanchForeman.cs
@@ -11,6 +11,44 @@ namespace RepRancher
      */
     class RanchForeman
     {
+        /*
+         * Determines how much output is sent to screen
+         */
+        public static bool NoisyClient;
+        /*
+         * This is the APIKey to MakerFarm
+         */
+        string ClientAPIKey;
 
+        /*
+         * This is the ClientID used to access MakerFarm
+         */
+        int MakerFarmClientID;
+
+        /*
+         * This is the number of seconds RepRancher will wait before assuming a response isn't coming from conveyor for blocking commands.
+         */
+        int ConveyorReplyTimeout;
+
+        Uri uri;
+        Uri ISpyUri;
+        Uri DoTellUri;
+        Uri ISayUri;
+        Uri TakeThis;
+
+        /*
+         * FileStream to Associated Conveyor Error Log
+         */
+        System.IO.FileStream ostrm;
+
+        /*
+         * StreamWriter used to communicate log errors to the file.
+         */
+        System.IO.StreamWriter errorLog;
+
+        /*
+         * This Timer is used to flush errors to the text file in a timely manner, to make sure that a crash doesnt get missed.
+         */
+        System.Timers.Timer ErrorFlush;
     }
 }
---
+++ RepRancher/RancherBrand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace RepRancher
+{
+
+    public class RancherBrandCollection : List<RancherBrand>
+    {
+        public RancherBrandCollection() : base() { }
+        public RancherBrandCollection(IEnumerable<RancherBrand> ListToCopy)
+            : this()
+        {
+            this.AddRange(ListToCopy);
+        }
+    }
+
+    [Serializable]
+    public class RancherBrand
+    {
+        public bool Enabled { get; set; }
+        public RancherType Type { get; set; }
+        public string PrintTemporaryFileStoragePath { get; set; }
+        public int PrinterReplyTimeout { get; set; }
+        public string IPAddress { get; set; }
+        public int PortNumber { get; set; }
+        public string AdvancedConfigurationString { get; set; }
+    }
+
+ 
+
+}
