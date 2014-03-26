32952390bb945d30f317a3e82cf6920bfea7483a
Patrick McMorran <patrick.mcmorran@yale.edu>
2014-03-26 17:08:36 -0400

    Unified Service and Main into same exe. First revision of working 2.4.1

---
+++ RepRancher/RepRancherService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepRancher
+{
+    public partial class RepRancherService : System.ServiceProcess.ServiceBase
+    {
+        RanchForeman Foreman;
+        System.Timers.Timer t;
+        public RepRancherService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            t = new System.Timers.Timer(5000);
+            t.Elapsed += new System.Timers.ElapsedEventHandler(startup);
+            t.Start();
+        }
+
+        public void Die(object source, System.Timers.ElapsedEventArgs e)
+        {
+            Environment.FailFast(null);
+        }
+
+        public void startup(object source, System.Timers.ElapsedEventArgs e)
+        {
+            t.Stop();
+            t.Enabled = false;
+            Foreman = new RanchForeman();
+        }
+
+        protected override void OnStop()
+        {
+            System.Timers.Timer d = new System.Timers.Timer(1000);
+            d.Elapsed += new System.Timers.ElapsedEventHandler(Die);
+            d.Enabled = true;
+            d.Start();
+        }
+
+        private void InitializeComponent()
+        {
+            // 
+            // RepRancherService
+            // 
+            this.ServiceName = "RepRancherService";
+
+        }
+    }
+}
