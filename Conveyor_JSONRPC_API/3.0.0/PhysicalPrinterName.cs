96faf5b7d2c7461931f9c5710285e2c5792531f4
Patrick McMorran <patrick.mcmorran@yale.edu>
2014-04-07 12:46:06 -0400

    Corrected PhysicalPrinterName to account for leading zeros

--- Conveyor_JSONRPC_API/3.0.0/PhysicalPrinterName.cs
+++ Conveyor_JSONRPC_API/3.0.0/PhysicalPrinterName.cs
@@ -9,13 +9,23 @@ namespace Conveyor_JSONRPC_API._3._0._0
     public class ConveyorPhysicalPrinterName
     {
         public string iserial { get; set; }
-        public int pid { get; set; }
+        public int pid { get; set; } //4 digits
         public string port_type { get; set; }
-        public int vid { get; set; }
+        public int vid { get; set; } //4 digits
 
         public string GetMachine_Hash()
         {
-            return string.Concat(vid.ToString("X"), ":", pid.ToString("X"), ":", iserial);
+            string PID = pid.ToString("X");
+            for (int i = PID.Length; i < 4; i++)
+            {
+                PID = string.Concat("0", PID);
+            }
+            string VID = vid.ToString("X");
+            for (int i = VID.Length; i < 4; i++)
+            {
+                VID = string.Concat("0", VID);
+            }
+            return string.Concat(VID, ":", PID, ":", iserial);
         }
     }
 }
